f9661128599dbc7d3f6b5b0688f1970bc68edd70
Nelson Gregorio <nelson.gregorio@live.com.pt>
2019-12-03 08:42:04 +0000

    Database structure cleanup

--- Projeto/database/testQuery.sql
+++
@@ -1,9 +0,0 @@
-SELECT apelido FROM Utilizador
-WHERE nome == "Nelson";
-
-SELECT casa_desc, data_inicio_a, data_fim_a FROM Casa
-NATURAL JOIN Arrendamento
-WHERE casaID == casa AND utilizador == 'afoxys@gmail.com';
-
-SELECT preco_diario, rua FROM Casa
-WHERE proprietario == 'pingdoce23@gmail.com';
